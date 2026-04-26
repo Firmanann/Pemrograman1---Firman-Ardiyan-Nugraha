2cb5f607bf1ad4fce80b66c7949f029d6e89324f
Firmanann <firmanardiyan02@gmail.com>
2026-04-26 20:55:54 +0700

    Create : tugas P2

--- P2_FirmanArdiyanNugraha/src/tugas/Tugas1.java
+++ P2_FirmanArdiyanNugraha/src/tugas/Tugas1.java
@@ -1,5 +1,32 @@
 package tugas;
 
+import java.util.Scanner;
+
 public class Tugas1 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String username;
+        String email;
+        int pin;
+
+        //Input
+        System.out.println("Input Username : ");
+        username = input.nextLine();
+
+        System.out.println("Input Email : ");
+        email = input.nextLine();
+
+        System.out.println("Input PIN 6 Digit (Angka) : ");
+        pin = input.nextInt();
+
+
+        //Output
+        System.out.println("Username : " + username);
+        System.out.println("Email    : " + email);
+        System.out.println("PIN      : " + pin);
+    }
 
 }
